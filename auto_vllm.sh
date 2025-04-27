ca5a0bb068c7046cfef68b6ae8e7043222525811
Parsa Bakhtiari <38098107+parssky@users.noreply.github.com>
2025-04-27 15:10:44 +0330

    Update auto_vllm.sh

--- auto_vllm.sh
+++ auto_vllm.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# 
-cd /workspace/
-# Cause the script to exit on failure.
-set -eo pipefail
-
-# Activate the main virtual environment
-. /venv/main/bin/activate
-
-# Add huggingface token for Meta
-export HUGGING_FACE_HUB_TOKEN=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
-
 # Run llama-3.1-8B-instruct in background
 vllm serve meta-llama/Llama-3.1-8B-Instruct --dtype half  --max-model-len 2000 &
 
