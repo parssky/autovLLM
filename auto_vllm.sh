a453fb28656e3935d380b410642488611b298639
Parsa Bakhtiari <38098107+parssky@users.noreply.github.com>
2025-04-29 17:29:39 +0330

    Update auto_vllm.sh

--- auto_vllm.sh
+++ auto_vllm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export HUGGING_FACE_HUB_TOKEN="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"
+export HUGGING_FACE_HUB_TOKEN="hf_LKLBwcEVTcvoxfEIXKOJRImwtJKlGzIoT"
 # Run llama-3.1-8B-instruct in background
 vllm serve meta-llama/Llama-3.1-8B-Instruct --dtype half  --max-model-len 2000 &
 
